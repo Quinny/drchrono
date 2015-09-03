9327286d6fd5b66d41592d8db20a68e23a47ea02
Quinn Perfetto <thequinnftw@gmail.com>
2015-09-03 10:36:14 -0400

    removed testing statement

--- birthday_wisher/utils.py
+++ birthday_wisher/utils.py
@@ -18,9 +18,9 @@ def date_range(d1, d2):
 # gets todays birthdays for the given doctor
 # also adds nessesary data to each user
 def todays_birthdays(doctor):
-    #patients = filter(is_birthday, api.get_patients(doctor))
+    patients = filter(is_birthday, api.get_patients(doctor))
     # just for testing
-    patients = filter(lambda x: x["date_of_birth"] is not None, api.get_patients(doctor))
+    #patients = filter(lambda x: x["date_of_birth"] is not None, api.get_patients(doctor))
     for p in patients:
         p["has_contact"] = p["home_phone"] or p["email"] or p["cell_phone"]
     return patients
